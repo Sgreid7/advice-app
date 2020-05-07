c6b9bb05a477367cd5e71c0932e21ea6145d61f3
Sam Greider <sam.greider7@gmail.com>
2020-05-07 12:18:35 -0400

    styled advice component and card

--- src/components/randomAdvice.jsx
+++ src/components/randomAdvice.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import styled from "styled-components"
+import Mountains from "../images/mountains.jpg"
 
 const advice = ({ advice }) => {
   return (
-    <Card>
-      <h1>{advice}</h1>
-    </Card>
+    <CardWrapper>
+      <div className="card">
+        <h1>{advice}</h1>
+        <button className="submit">One advice, please!</button>
+      </div>
+    </CardWrapper>
   );
 }
 
 export default advice;
 
-const Card = styled.section`
+const CardWrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${Mountains});
+  background-size: cover;
+  background-position: center;
+
+  .card {
+    color: #fff;
+    font-size: 1.2rem;
+    font-style: italic;
+    text-align: center;
+    letter-spacing: 0.03rem;
+    background: rgba(0, 0, 0, 0.4);
+    width: 40vw;
+    height: 20vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 0.1rem solid deepskyblue;
+    border-radius: 1rem;
+    padding: 2rem;
+    text-shadow: 0.05rem 0.05rem 0.05rem deepskyblue;
+    box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.9);
+  }
 `
+
